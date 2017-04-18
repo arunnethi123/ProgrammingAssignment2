d7ee53f6d0d8536789443585f37ba440e6eadf49
arunnethi123 <arunnethi123@gmail.com>
2017-04-17 23:05:34 -0400

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#Creating function to 1)set matrix 2)get matrix which returns matrix 3)setInv to store the inverse 4) getInv to 
+#return inverse matrix.
+makeCacheMatrix <- function(mat_data=matrix())
+{
+  inv<-NULL
+  set<-function(y)
+  {
+    mat_data<<-y
+    inv<<-NULL
+    mat_data
+  }
+  get<-function() mat_data
+  
+  setInv<-function(i)
+  {
+    inv<<-i
+  }
+  getInv<-function() inv
+  
+  list(set=set,get=get,setInv=setInv,getInv=getInv)
+  
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#Creating function to calculate inverse and return it if it is not created else returns the existing inverse 
+#matrix with a message.
+cacheSolve<- function(mcm,...)
+{
+  inv<-mcm$getInv()
+  if(!is.null(inv))
+  {
+    message("getting inverse matrix data")
+    return(inv)
+  }
+  mat<-mcm$get()
+  inv=solve(mat,...)
+  mcm$setInv(inv)
+  return(inv)
+  
 }
+
+
+#Example Input with result: l=makeCacheMatrix(matrix(c(1,2,3,4),2,2))
+# cacheSolve(l)
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+# cacheSolve(l)
+#getting inverse matrix data
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+
